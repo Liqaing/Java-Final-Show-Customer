095fa2fe24d0381b8fdee37a67e21575cd0694ac
Rithy <huotrithy01@gmail.com>
2024-07-18 20:15:58 +0700

    First

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class Customer {
@@ -6,27 +10,111 @@ public class Customer {
     public static Connection conn;
     public static PreparedStatement ps;
 
+    private static JTextField idField;
+    private static JTextField lastNameField;
+    private static JTextField firstNameField;
+    private static JTextField phoneField;
+
     public static void main(String[] args){
 
         try {
             conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/mydb", "root", "rick2908");
-            st = conn.createStatement();
-
-            rs = st.executeQuery("SELECT * FROM Product");
+            st = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = st.executeQuery("SELECT * FROM Customer");
             System.out.println(rs);
-            while (rs.next()) {
-                int id = rs.getInt("ID");
-                String name = rs.getString("Name");
-                double pricePerUnit = rs.getDouble("PricePerUnit");
-                int isActiveForSale = rs.getBoolean("IsActiveForSale") ? 1 : 0;
-
-                System.out.println("ID: " + id + ", Name: " + name + ", Price Per Unit: " + pricePerUnit + ", Is Active For Sale: " + isActiveForSale);
-            }
         }
         catch (Exception e) {
             System.out.println("Error");
             System.out.println(e.getMessage());
         }
+
+        JFrame frame = new JFrame("Customer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new GridLayout(5, 2));
+
+        frame.add(new JLabel("ID:"));
+        idField = new JTextField(10);
+        idField.setEditable(false);
+        frame.add(idField);
+
+        frame.add(new JLabel("Last Name:"));
+        lastNameField = new JTextField(10);
+        lastNameField.setEditable(false);
+        frame.add(lastNameField);
+
+        frame.add(new JLabel("First Name:"));
+        firstNameField = new JTextField(10);
+        firstNameField.setEditable(false);
+        frame.add(firstNameField);
+
+        frame.add(new JLabel("Phone:"));
+        phoneField = new JTextField(10);
+        phoneField.setEditable(false);
+        frame.add(phoneField);
+
+        JButton prevButton = new JButton("Previous");
+        prevButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (rs.previous()) {
+                        int id = rs.getInt("customer_id");
+                        String lastName = rs.getString("customer_last_name");
+                        String firstName = rs.getString("customer_first_name");
+                        String phone = rs.getString("customer_phone");
+
+                        idField.setText(String.valueOf(id));
+                        lastNameField.setText(lastName);
+                        firstNameField.setText(firstName);
+                        phoneField.setText(phone);
+                    }
+                }
+                catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        });
+        frame.add(prevButton);
+
+        JButton nextButton = new JButton("Next");
+        nextButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (rs.next()) {
+                        int id = rs.getInt("customer_id");
+                        String lastName = rs.getString("customer_last_name");
+                        String firstName = rs.getString("customer_first_name");
+                        String phone = rs.getString("customer_phone");
+
+                        idField.setText(String.valueOf(id));
+                        lastNameField.setText(lastName);
+                        firstNameField.setText(firstName);
+                        phoneField.setText(phone);
+                    }
+                }
+                catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        });
+        try {
+            int id = rs.getInt("customer_id");
+            String lastName = rs.getString("customer_last_name");
+            String firstName = rs.getString("customer_first_name");
+            String phone = rs.getString("customer_phone");
+
+            idField.setText(String.valueOf(id));
+            lastNameField.setText(lastName);
+            firstNameField.setText(firstName);
+            phoneField.setText(phone);
+        }
+
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        frame.add(nextButton);
+        frame.setVisible(true);
 
     }
 }
